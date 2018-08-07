6890f9ff77a7798f61a02031386bed9112ea5202
Ellis Birt <ebirt@practice-engine.com>
2018-08-07 21:38:23 +0100

    more tests

--- src/dirtyFlag2/tests.ts
+++ src/dirtyFlag2/tests.ts
@@ -1,10 +1,6 @@
-﻿QUnit.test("hello test", function (assert) {
-    assert.ok(1 == 1, "Passed!");
-});
-
-QUnit.test("Simple extend test", function (assert) {
+﻿QUnit.test("Simple extend test", function (assert) {
     var name: KnockoutObservable<string>;
-    name = ko.observable("Joe Smith").extend({ dirtyFlag: "" });
+    name = ko.observable("Joe Smith").extend({ dirtyFlag: {} });
 
     assert.ok(name["isDirty"]() === false, "Initialised as clean");
 
@@ -16,21 +12,76 @@ QUnit.test("Simple extend test", function (assert) {
     name("Mark");
     assert.ok(name["isDirty"]() === true, "Updated again Now Dirty");
 });
-/*
-QUnit.test("Extend With Key, but no dirtyFlag2 instance", function (assert) {
-    var name: KnockoutObservable<string>;
-    assert.throws(function() {
-        var name = ko.observable("Joe Smith").extend({ dirtyFlag: { key: "name" } });
-    },
-    "Exception thrown if extending with key that does not have matching dirtyFlag2");
+
+QUnit.test("Simple extend with object", function (assert) {
+    var name: KnockoutObservable<object>;
+    assert.throws(
+        function () {
+            name = ko.observable({}).extend({ dirtyFlag: {} });
+        }, "Exception extending object observable"
+    );
 });
-*/
 QUnit.test("Extend With empty Key", function (assert) {
     var name: KnockoutObservable<string>;
-    assert.throws(function () {
-            var name = ko.observable("Joe Smith").extend({ dirtyFlag: { key: "" } });
+    assert.throws(
+        function () {
+        var name = ko.observable("Joe Smith").extend({ dirtyFlag: { key: "" } });
         },
-
         "Exception thrown if extending with empty key"
     );
+});
+
+QUnit.test("Extend With Key, but no dirtyFlag2 instance", function (assert) {
+    var name: KnockoutObservable<string>;
+    assert.throws(
+        function () {
+            var name = ko.observable("Joe Smith").extend({ dirtyFlag: { key: "Tests1.name" } });
+        },
+        "Exception thrown if extending with key that does not have matching dirtyFlag2"
+    );
+});
+
+QUnit.test("Extend With Key, existing dirtyFlag2 instance, test simple functionality", function (assert) {
+    var name: KnockoutObservable<string>;
+    var df = new dirtyFlag2("Tests");
+    name = ko.observable("Joe Smith").extend({ dirtyFlag: { key: "Tests.name" } });
+    assert.ok(name["isDirty"]() === false, "Initialised as clean");
+
+    name("John");
+    assert.ok(name["isDirty"]() === true, "Updated Now Dirty");
+    name["reset"]();
+    assert.ok(name["isDirty"]() === false, "Reset OK");
+    assert.ok(name() === "John", "Value Unchanged OK");
+    name("Mark");
+    assert.ok(name["isDirty"]() === true, "Updated again Now Dirty");
+    df.dispose();
+});
+QUnit.test("Extend With Key, existing dirtyFlag2 instance, duplicate key", function (assert) {
+    var name: KnockoutObservable<string>;
+    var df = new dirtyFlag2("Tests2");
+    name = ko.observable("Joe Smith").extend({ dirtyFlag: { key: "Tests2.name" } });
+    assert.throws(
+        function () {
+            var name2 = ko.observable("Joe Smith").extend({ dirtyFlag: { key: "Tests2.name" } });
+        },
+        "trying to re-register key"
+    );
+    df.dispose();
+});
+
+///Array observable tests
+
+QUnit.test("Simple array extend test", function (assert) {
+    var names: KnockoutObservableArray<string>;
+    names = ko.observableArray(["Joe Smith"]).extend({ dirtyFlag: {} });
+
+    assert.ok(names["isDirty"]() === false, "Initialised as clean");
+
+    names.push("John");
+    assert.ok(names["isDirty"]() === true, "Updated Now Dirty");
+    name["reset"]();
+    assert.ok(names["isDirty"]() === false, "Reset OK");
+    //assert.ok(name() === "John", "Value Unchanged OK");
+    names(["Mark"]);
+    assert.ok(name["isDirty"]() === true, "Updated again Now Dirty");
 });
--- src/dirtyFlag2/wwwroot/tests.js
+++ src/dirtyFlag2/wwwroot/tests.js
@@ -1,9 +1,6 @@
-QUnit.test("hello test", function (assert) {
-    assert.ok(1 == 1, "Passed!");
-});
 QUnit.test("Simple extend test", function (assert) {
     var name;
-    name = ko.observable("Joe Smith").extend({ dirtyFlag: "" });
+    name = ko.observable("Joe Smith").extend({ dirtyFlag: {} });
     assert.ok(name["isDirty"]() === false, "Initialised as clean");
     name("John");
     assert.ok(name["isDirty"]() === true, "Updated Now Dirty");
@@ -13,19 +10,45 @@ QUnit.test("Simple extend test", function (assert) {
     name("Mark");
     assert.ok(name["isDirty"]() === true, "Updated again Now Dirty");
 });
-/*
-QUnit.test("Extend With Key, but no dirtyFlag2 instance", function (assert) {
-    var name: KnockoutObservable<string>;
-    assert.throws(function() {
-        var name = ko.observable("Joe Smith").extend({ dirtyFlag: { key: "name" } });
-    },
-    "Exception thrown if extending with key that does not have matching dirtyFlag2");
+QUnit.test("Simple extend with object", function (assert) {
+    var name;
+    assert.throws(function () {
+        name = ko.observable({}).extend({ dirtyFlag: {} });
+    }, "Exception extending object observable");
 });
-*/
 QUnit.test("Extend With empty Key", function (assert) {
     var name;
     assert.throws(function () {
         var name = ko.observable("Joe Smith").extend({ dirtyFlag: { key: "" } });
     }, "Exception thrown if extending with empty key");
+});
+QUnit.test("Extend With Key, but no dirtyFlag2 instance", function (assert) {
+    var name;
+    assert.throws(function () {
+        var name = ko.observable("Joe Smith").extend({ dirtyFlag: { key: "Tests1.name" } });
+    }, "Exception thrown if extending with key that does not have matching dirtyFlag2");
+});
+QUnit.test("Extend With Key, existing dirtyFlag2 instance, test simple functionality", function (assert) {
+    var name;
+    var df = new dirtyFlag2("Tests");
+    name = ko.observable("Joe Smith").extend({ dirtyFlag: { key: "Tests.name" } });
+    assert.ok(name["isDirty"]() === false, "Initialised as clean");
+    name("John");
+    assert.ok(name["isDirty"]() === true, "Updated Now Dirty");
+    name["reset"]();
+    assert.ok(name["isDirty"]() === false, "Reset OK");
+    assert.ok(name() === "John", "Value Unchanged OK");
+    name("Mark");
+    assert.ok(name["isDirty"]() === true, "Updated again Now Dirty");
+    df.dispose();
+});
+QUnit.test("Extend With Key, existing dirtyFlag2 instance, duplicate key", function (assert) {
+    var name;
+    var df = new dirtyFlag2("Tests2");
+    name = ko.observable("Joe Smith").extend({ dirtyFlag: { key: "Tests2.name" } });
+    assert.throws(function () {
+        var name2 = ko.observable("Joe Smith").extend({ dirtyFlag: { key: "Tests2.name" } });
+    }, "trying to re-register key");
+    df.dispose();
 });
 //# sourceMappingURL=tests.js.map
